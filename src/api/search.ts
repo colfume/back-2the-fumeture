08a76a8188713d3b0e97f6d3a9f0e57d96e6753d
jeongjae <rchmas@gmail.com>
2021-07-05 02:45:03 +0900

    Feat: search_filter_mood 작업중

--- src/api/search.ts
+++ src/api/search.ts
@@ -22,5 +22,40 @@ router.get("/getkeyword", async (req, res) => {
   }
 });
 
+router.get("/filter/:moodName", async (req, res) => {
+  try {
+      
+      const moodId = await Mood.findOne(
+        { mood_name : req.params.moodName },
+        { attributes: ['_id'] }
+      );
+
+      const filtered_perfume = await Perfume.find(
+        { mood_id: moodId },
+        { attributes: ['perfume_name','mood_id']}
+      ).populate('mood', ['mood_name']);
+
+      console.log(filtered_perfume);
+      res.json({moods: filtered_perfume, message: "무드 향수 정보 불러오기 성공"});
+
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const { perfume_name, brand, color_id, style_id, mood_id, description, top, middle, base, perfume_img, palette_id} = req.body;
+    const perfume = new Perfume({
+      perfume_name, brand, color_id, style_id, mood_id, description, top, middle, base, perfume_img, palette_id
+    })
+
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+  }
+});
+
 
 module.exports = router;
