9bfb0c4ce9f728854105f66749ee2ae61a8740e6
jeongjae <rchmas@gmail.com>
2021-07-06 04:39:24 +0900

    Feat: Filter_mood 작업중

--- src/api/search.ts
+++ src/api/search.ts
@@ -22,4 +22,31 @@ router.get("/getkeyword", async (req, res) => {
   }
 });
 
+router.get("/filter/:moodName", async (req, res) => {
+  try {
+
+      let moodId = await Mood.findOne(
+        { mood_name : req.params.moodName },
+        { attributes: ['_id']}
+      );
+      
+      moodId = moodId._id;
+      console.log(moodId);
+
+      let filtered_perfume = await Perfume.find(
+        { "moods.0.mood1" : moodId },
+        { attributes: ['_id']}
+      );
+      //.populate('mood', ['mood_name']);
+
+
+      res.json({moods: filtered_perfume, message: "무드 향수 정보 불러오기 성공"});
+
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+  }
+});
+
+
 module.exports = router;
