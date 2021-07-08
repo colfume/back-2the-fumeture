efdcbe838a3535725c932c125436594621837f93
sssua-0928 <hansua990928@gmail.com>
2021-07-08 18:47:50 +0900

    컬퓸 컨트롤러

--- src/api/colfumetest.controller.spec.ts
+++ src/api/colfumetest.controller.spec.ts
@@ -24,7 +24,7 @@ router.post("/colfumetest", async (req, res) => {
   }
   // 3. 질문에 따른 컬러 가중치를 계산합니다.
   // 3. 컬러를 선언해줍니다. (컬러 총 8개)
-  const red : Number | String = 0;
+  const red : number | String = 0;
   const red2 = 0;
   const orange : number = 0;
   const vanila : number = 0;
@@ -35,16 +35,105 @@ router.post("/colfumetest", async (req, res) => {
   const white : number = 0;
 
   //const colorResult = { red, orange, vanila, green, sky, purple, pink, white } : Array<Number> = 0;
-
+  // 질문 1번
   if( answer1 === 1 ){
     vanila + 3;
     orange + 3;
   }
-  else if(answer1 === 2){
-
+  else if( answer1 === 2 ){
+    red + 1;
+    green + 1;
+  };
+  // 질문 2번
+  if( answer2 === 1 ){
+    red + 3;
+    white + 3;
   }
+  else if( answer2 === 2 ){
+    orange + 3;
+    sky + 3;
+    green + 3;
+    purple + 3;
+  };
+  // 질문 3번
+  if( answer3 === 1 ){
+    red + 3;
+    purple + 3;
+    green + 5;
+  }
+  else if( answer3 === 2 ){
+    vanila + 3;
+    orange + 3;
+    pink + 3;
+  };
+  // 질문 4번
+  if( answer4 === 1 ){
+    green + 5;
+    red + 3;
+    purple + 3;
+  }
+  else if( answer4 === 2 ){
+    vanila + 3;
+    orange + 3;
+    pink + 3;
+  };
+  // 질문 5번
+  if( answer5 === 1 ){
+    purple + 3;
+    red + 1; 
+  }
+  else if( answer5 === 2 ){
+    orange + 3;
+    sky + 3;
+    white + 3;
+    pink + 1;
+    vanila + 1;
+  }
+  // 질문 6번
+  if( answer6 === 1 ){
+    red + 3; 
+    green + 3;
+    purple + 3;
+    sky + 3;
+  }
+  else if( answer6 === 2 ){
+    orange + 3;
+    white + 3;
+    pink + 3;
+  };
+  // 질문 7번
+  if( answer7 === 1 ){
+    green + 5;
+    white + 1;
+  }
+  else if( answer7 === 2 ){
+    sky + 3;
+    purple + 3;
+  }
+
+  // 색깔 배열 만들기
+  const colorPalette = [ red, orange, vanila, green, sky, purple, pink, white ];
+
   try {
-    // 4. 최종적으로 가장 점수가 높은 컬러를 반환합니다.
+    // 4. 최종적으로 가장 점수가 높은 컬러를 찾습니다.
+    const max_color = Math.max.apply( null, colorPalette );
+    console.log(max_color);
+    // 5. Palette 테이블에서 이름이 같은 컬럼을 찾고, 반환합니다. 
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(400).json({
+        errors: [{ msg: "Invalid Credentials" }],
+      });
+    }
+    
+    const result = await Palette.findOne({
+      palette_name,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+    res.json(profile);
 
     // 4-1. 컬러반환이 잘 됐을 경우
     return res.status(200).send("결과를 가져와");
