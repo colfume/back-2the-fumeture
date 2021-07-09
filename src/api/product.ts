5bf42d54079fef2935c6ba18316773153025480a
최다인 <twa05157@gmail.com>
2021-07-10 01:26:32 +0900

    Feat: product page

--- src/api/product.ts
+++ src/api/product.ts
@@ -19,4 +19,26 @@ router.get("/home/palette", async (req, res) => {
   }
 });
 
+router.get("/home/product", async (req, res) => {
+  try {
+    const result = await Perfume.find({
+      attributes: ['_id', 'perfume_name']
+    })
+    .populate({
+      path: "moods",
+      populate: [{
+        path: "mood1",
+        options: { retainNullValues: true }
+      }]
+    });
+    if (!result) {
+      return res.status(400).send("필요한 값이 없습니다.");
+    }
+    res.json({ result, message: "프로덕트 페이지 불러오기 성공" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("서버 내부 에러입니다.");
+  }
+});
+
 module.exports = router;
