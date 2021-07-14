fad2e1214eaaa0c4e4c58f563da1ea6c5c3fae22
Kakao_Ent <kakao_ent@Kakao-Entui-MacBookPro.local>
2021-07-14 17:57:31 +0900

    Feat: all perfume product

--- src/api/product.ts
+++ src/api/product.ts
@@ -4,6 +4,34 @@ import Palette from "../models/Palette";
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  try {
+    const result = await Perfume.find()
+    .select(["perfume_name", "perfume_img"])
+    .populate({
+      path: "moods",
+      populate: [{
+        path: "mood1",
+        options: { retainNullValues: true }
+      },
+      {
+        path: "mood2",
+        options: { retainNullValues: true }
+      },
+      {
+        path: "mood3",
+        options: { retainNullValues: true }
+      }]
+    });
+    if (!result) {
+      return res.status(400).send("필요한 값이 없습니다.");
+    }
+    res.status(200).json({ data: result, message: "프로덕트 페이지 불러오기 성공했습니다." });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("서버 내부 에러입니다.");
+  }
+});
 router.get("/:paletteName", async (req, res) => {
   try {
     let paletteId = await Palette.findOne(
@@ -42,7 +70,6 @@ router.get("/:paletteName", async (req, res) => {
     res.status(500).send("서버 내부 에러입니다.");
   }
 });
-
 router.get("/detail/:perfumeName", async (req, res) => {
   try {
     let perfumeId = await Perfume.findOne(
